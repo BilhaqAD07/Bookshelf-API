704f4d911e5cbd2edfca0fa91ae2aa0bcdc490b1
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2024-12-11 23:42:50 +0700

    Initial push Bookshelf-API

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { addBooksHandler, deleteBookByIdHandler, editBookByIdHandler, getAllBooksHandler, getBooksByIdHandler } from './handler.js';
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+import process from 'process';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server is running on ${server.info.uri}`);
+};
+
+init();
