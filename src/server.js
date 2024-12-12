c9705ebab9f9a2d231b45344cb6adf480164910e
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2024-12-12 12:03:38 +0700

    Fix: change port value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import process from 'process';
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
